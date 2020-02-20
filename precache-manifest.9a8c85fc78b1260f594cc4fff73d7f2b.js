72ed1c419ca92bc33f3d7680fb3e2746f5e171c2
miljanabogdanovic1991 <bogdanovic.miljana91@gmail.com>
2020-02-20 22:54:47 +0100

    Updates

---
+++ precache-manifest.9a8c85fc78b1260f594cc4fff73d7f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1d665061305f2ab396a8f640a9d6b1",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "ab07a40bb19f550ab952",
+    "url": "/hacker-news/static/css/main.c20d5fbf.chunk.css"
+  },
+  {
+    "revision": "dcd6f25b66b8d4e11b03",
+    "url": "/hacker-news/static/js/2.d65705c1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/hacker-news/static/js/2.d65705c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab07a40bb19f550ab952",
+    "url": "/hacker-news/static/js/main.09dc4aa6.chunk.js"
+  },
+  {
+    "revision": "0c5fd55c54204060d675",
+    "url": "/hacker-news/static/js/runtime-main.1bc41a99.js"
+  }
+]);
